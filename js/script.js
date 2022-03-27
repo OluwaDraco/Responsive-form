bfdf0324af5c3da12a64e88c5ded725eec47e9cf
olushola oludipe <shotown101@gmail.com>
2022-03-27 16:30:56 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,46 +1,81 @@
-const nameField = document.getElementById('name');
-const otherJobField = document.getElementById('other-job-role');
-const jobField = document.getElementById('title');
+const nameField = document.getElementById("name");
+const otherJobField = document.getElementById("other-job-role");
+const jobField = document.getElementById("title");
+/**
+ *
+ * @param {*} show  div to display
+ * @param {*} hide  div to hide
+ * @param {*} hide2 div to hide
+ */
+function showUI(show, hide, hide2) {
+  show.style.display = "block";
+  hide2.style.display = "none";
+  hide.style.display = "none";
+}
 
 nameField.focus();
-otherJobField.style.display = 'none';
+otherJobField.style.display = "none";
 
-jobField.addEventListener('change',(e)=>{
-    const list = document.querySelectorAll('#title option');
+jobField.addEventListener("change", (e) => {
+  const list = document.querySelectorAll("#title option");
 
-    for(let job of list){
-        if(e.target.value === 'other'){
-            otherJobField.style.display = ''
-            
-        }
-        else{
-            otherJobField.style.display = 'none'
-        }
-    }
+  for (let job of list) {
+	if (e.target.value === "other") {
+	  otherJobField.style.display = "";
+	} else {
+	  otherJobField.style.display = "none";
+	}
+  }
 });
 /**
- * this is where the problem is
+ *
  */
-const design = document.getElementById('design');
-const color = document.getElementById('color');
+const design = document.getElementById("design");
+const color = document.getElementById("color");
 color.disabled = true;
 
+/**
+ * when a design is picked only color options of that option is displayed the rest are hidden.
+ */
+design.addEventListener("change", (e) => {
+  const colorOptions = document.querySelectorAll("#color option");
+  color.disabled = false;
+  for (let colors of colorOptions) {
+	colors.style.display = "none";
+	if (e.target.value === colors.getAttribute("data-theme")) {
+	  colors.style.display = "";
+	}
+  }
+});
 
+const activityList = document.getElementById("activities");
+const activityCost = document.getElementById("activities-cost");
+let totalCost = 0;
 
-design.addEventListener('change', (e)=>{
-    const colorOptions = document.querySelectorAll('#color option');
-    color.disabled = false;
-    for(let colors of colorOptions){
-        colors.style.display ='none'
-        if(e.target.value === colors.getAttribute('data-theme')){
-            console.log('true')
-            colors.style.display =''
-          
+activityList.addEventListener("change", (e) => {
+  let cost = e.target.getAttribute("data-cost");
+  cost = +cost;
+  if (e.target.checked == true) {
+	totalCost += cost;
+  }
+  if (e.target.checked !== true) {
+	totalCost -= cost;
+	console.log(totalCost);
+  }
+  activityCost.innerHTML = `Total: $${totalCost}`;
+});
+const payment = document.querySelector("#payment");
+const creditCard = document.getElementById("credit-card");
+const payPal = document.getElementById("paypal");
+const bitcoin = document.getElementById("bitcoin");
+const paymentMethod = document
+  .querySelector("#payment option:nth-child(2)")
+  .setAttribute("selected", "true");
+payPal.style.display = "none";
+bitcoin.style.display = "none";
 
-
-
-           
-        }
-    }
-})
-
+payment.addEventListener("change", (e) => {
+  if (e.target.value === "credit-card") showUI(creditCard, payPal, bitcoin);
+  if (e.target.value === "paypal") showUI(payPal, bitcoin, creditCard);
+  if (e.target.value === "bitcoin") showUI(bitcoin, payPal, creditCard);
+});
