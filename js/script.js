d7743f054a135fc4da3390316f67544d36ffca77
olushola oludipe <shotown101@gmail.com>
2022-03-28 22:21:15 -0400

    added form validation

--- js/script.js
+++ js/script.js
@@ -8,24 +8,24 @@ const jobField = document.getElementById("title");
  * @param {*} hide2 div to hide
  */
 function showUI(show, hide, hide2) {
-  show.style.display = "block";
-  hide2.style.display = "none";
-  hide.style.display = "none";
+    show.style.display = "block";
+    hide2.style.display = "none";
+    hide.style.display = "none";
 }
 
 nameField.focus();
 otherJobField.style.display = "none";
 
 jobField.addEventListener("change", (e) => {
-  const list = document.querySelectorAll("#title option");
+    const list = document.querySelectorAll("#title option");
 
-  for (let job of list) {
-	if (e.target.value === "other") {
-	  otherJobField.style.display = "";
-	} else {
-	  otherJobField.style.display = "none";
-	}
-  }
+    for (let job of list) {
+        if (e.target.value === "other") {
+            otherJobField.style.display = "";
+        } else {
+            otherJobField.style.display = "none";
+        }
+    }
 });
 /**
  *
@@ -38,14 +38,14 @@ color.disabled = true;
  * when a design is picked only color options of that option is displayed the rest are hidden.
  */
 design.addEventListener("change", (e) => {
-  const colorOptions = document.querySelectorAll("#color option");
-  color.disabled = false;
-  for (let colors of colorOptions) {
-	colors.style.display = "none";
-	if (e.target.value === colors.getAttribute("data-theme")) {
-	  colors.style.display = "";
-	}
-  }
+    const colorOptions = document.querySelectorAll("#color option");
+    color.disabled = false;
+    for (let colors of colorOptions) {
+        colors.style.display = "none";
+        if (e.target.value === colors.getAttribute("data-theme")) {
+            colors.style.display = "";
+        }
+    }
 });
 
 const activityList = document.getElementById("activities");
@@ -53,29 +53,96 @@ const activityCost = document.getElementById("activities-cost");
 let totalCost = 0;
 
 activityList.addEventListener("change", (e) => {
-  let cost = e.target.getAttribute("data-cost");
-  cost = +cost;
-  if (e.target.checked == true) {
-	totalCost += cost;
-  }
-  if (e.target.checked !== true) {
-	totalCost -= cost;
-	console.log(totalCost);
-  }
-  activityCost.innerHTML = `Total: $${totalCost}`;
+    let cost = e.target.getAttribute("data-cost");
+    cost = +cost;
+    if (e.target.checked == true) {
+        totalCost += cost;
+    }
+    if (e.target.checked !== true) {
+        totalCost -= cost;
+        console.log(totalCost);
+    }
+    activityCost.innerHTML = `Total: $${totalCost}`;
 });
 const payment = document.querySelector("#payment");
 const creditCard = document.getElementById("credit-card");
 const payPal = document.getElementById("paypal");
 const bitcoin = document.getElementById("bitcoin");
 const paymentMethod = document
-  .querySelector("#payment option:nth-child(2)")
-  .setAttribute("selected", "true");
+    .querySelector("#payment option:nth-child(2)")
+    .setAttribute("selected", "true");
 payPal.style.display = "none";
 bitcoin.style.display = "none";
 
 payment.addEventListener("change", (e) => {
-  if (e.target.value === "credit-card") showUI(creditCard, payPal, bitcoin);
-  if (e.target.value === "paypal") showUI(payPal, bitcoin, creditCard);
-  if (e.target.value === "bitcoin") showUI(bitcoin, payPal, creditCard);
+    if (e.target.value === "credit-card") showUI(creditCard, payPal, bitcoin);
+    if (e.target.value === "paypal") showUI(payPal, bitcoin, creditCard);
+    if (e.target.value === "bitcoin") showUI(bitcoin, payPal, creditCard);
+});
+
+/**
+ * VALIDATORS
+ */
+
+function usernameValidator(username) {
+    return /^[a-zA-Z]+$/.test(username);
+}
+
+function emailValidator(mail) {
+    return /^[a-zA-Z][a-zA-z0-9.]+@(gmail|yahoo|max).com$/.test(mail);
+}
+
+function cvvValidator(num) {
+    return /[0-9]{3}/.test(num);
+}
+
+function zipValidator(num) {
+    return /[0-9]{5}/.test(num);
+}
+function ccNumberValidator(num) {
+    return /[0-9]{13,16}/.test(num);
+}
+
+/**
+ * VALUE EXTRACTOR
+ */
+function valueExtract(rawData) {
+    return rawData.value;
+}
+
+const userForm = document.querySelector("form");
+const emailField = document.getElementById("email");
+const cvvField = document.getElementById("cvv");
+const ccNumberField = document.getElementById("cc-num");
+const zipField = document.getElementById("zip");
+
+userForm.addEventListener("submit", (event) => {
+    const nameInput = valueExtract(nameField);
+    const emailInput = valueExtract(emailField);
+    const cvvInput = valueExtract(cvvField);
+    const ccNumberInput = valueExtract(ccNumberField);
+    const zipInput = valueExtract(zipField);
+
+    if (
+        usernameValidator(nameInput && emailValidator(emailInput)) &&
+        totalCost > 0 &&
+        payment.value !== "credit-card"
+    ) {
+        console.log("first test past");
+    }
+
+    else if (
+        payment.value == "credit-card" &&
+        ccNumberValidator(ccNumberInput) &&
+        cvvValidator(cvvInput) &&
+        zipValidator(zipInput) &&
+        usernameValidator(nameInput && emailValidator(emailInput)) &&
+        totalCost > 0
+    ) {
+        console.log("second test past");
+    }
+
+    else{
+        event.preventDefault();
+    }
 });
